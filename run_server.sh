7e1827a9cfec347393e980c92ae14ff31d6fa4ff
jhertz <jh.furtado@campus.fct.unl.pt>
2023-04-20 16:10:21 +0100

    changed requests to use new method

--- run_server.sh
+++ run_server.sh
@@ -5,21 +5,37 @@ JAR_FILE=sd2223.jar
 IMAGE=sd2223-trab1-60198-61177
 NETWORK=sd-proj1
 PORT=8080
-EXPOSED="--expose $PORT -P"
+REST_SERVER="sd2223.trab1.servers.rest.RestServer"
+SOAP_SERVER="sd2223.trab1.servers.soap.SoapServer"
+SERVER=
 
-if [[ "$1" = "-b" ||  "$1" = "--build" ]]; then
-    echo "building project"
-    mvn compile assembly:single docker:build
+while true
+do
+  case $1 in
+    -b|--build)
+      echo "building project"
+      mvn compile assembly:single docker:build
+    ;;
+    -p|--port)
+      echo "exposing port... "
+      if  [[ "$2" =~ ^[0-9]+$ ]]  ; then
+        EXPOSED="-p $2:$PORT"
+        echo "using port: $2"
+        shift
+      else
+        EXPOSED="--expose $PORT -P"
+      fi
+     ;;
+    --soap)
+      echo "server: SOAP"
+      SERVER=$SOAP_SERVER
+      ;;
+    --rest) ;;
+    *)
+      break ;;
+  esac
     shift
-fi
-
-if [ "$1" = "-p" ]; then
-    [[ "$2" =~ ^[0-9]+$ ]] && IN_PORT=$2 && shift || IN_PORT=$PORT
-    echo "using port $IN_PORT"
-    EXPOSED="-p $IN_PORT:$PORT"
-    shift
-fi
-
+done
 
 if [ $# -lt 2 ] ; then
     echo "usage: $0 <domain> [ users | feeds ]"
@@ -27,9 +43,12 @@ if [ $# -lt 2 ] ; then
     exit 1
 fi
 
-#CMD="java -cp "$JAR_FILE" sd2223.trab1.servers.rest.RestServer $@"
-CMD="java -cp "$JAR_FILE" sd2223.trab1.servers.soap.SoapServer $@"
+if [ -z "$SERVER" ] ; then
+  echo "server: REST"
+  SERVER=$REST_SERVER
+fi
 
+CMD="java -cp "$JAR_FILE" $SERVER $*"
 echo "running: $*"
 docker run --rm -it --network "$NETWORK" $EXPOSED  "$IMAGE" $CMD
 
